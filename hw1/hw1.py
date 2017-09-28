4077325e782d3847ee0191fd8657adf8de589b3b
Gratisfo <31654704+Gratisfo@users.noreply.github.com>
2017-09-28 21:19:39 +0300

    Add files via upload

---
+++ hw1/hw1.py
@@ -0,0 +1,12 @@
+print ('Введите три числа')
+a = int(input())
+b = int(input())
+c = int(input())
+if a * b == c:
+    print (a, 'умножить на', b, 'равно', c) 
+else:
+    print (a, 'умножить на', b, 'не равно', c) 
+if a / b == c:
+    print (a, 'разделить на', b, 'равно', c) 
+else:
+    print (a, 'разделить на', b, ' не равно', c) 
