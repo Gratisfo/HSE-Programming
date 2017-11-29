c1cd69a3e282e2a615bd2dba9664f42c277ed020
Gratisfo <31654704+Gratisfo@users.noreply.github.com>
2017-11-29 21:22:00 +0300

    Create hw5.py

---
+++ hw5/hw5.py
@@ -0,0 +1,13 @@
+inp = []
+while True:
+    x = input('Введите латинское слово: ')
+    if x == '': break
+    else: inp.append(x)
+out = []
+for i in inp:
+  if i[-3:] == "tur" or i[-3:] == "ntur":
+    out.append(i)
+y = '\n'.join(out)
+with open("function.txt", "w", encoding="utf-8") as f:
+    f.write(y)
+  
