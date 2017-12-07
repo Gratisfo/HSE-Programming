311509901dc0a97521c3e2f8dbe9194d4c8b998a
Gratisfo <31654704+Gratisfo@users.noreply.github.com>
2017-12-07 11:56:10 +0300

    Create Control_1.py

---
+++ Control_1.py
@@ -0,0 +1,11 @@
+voc = []
+with open ('freq.txt', encoding ='utf-8') as f:
+    lines = f.readlines()
+    for line in lines:
+        cells = line.split()
+        if len(cells) >= 4 and cells[4] == 'перех':
+            voc.append(cells)
+            y = ' '.join(cells)
+            print(y)
+            
+ 
