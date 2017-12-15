38e11b2823ec3dad15325669df53348cbb34c209
Gratisfo <31654704+Gratisfo@users.noreply.github.com>
2017-12-15 16:21:49 +0300

    Update 3.py

--- bonus/3.py
+++ bonus/3.py
@@ -4,13 +4,13 @@ words = input()
 if words != '':
     words = words.split()
     for word in words:
-        if word[0] in vow:
+        if word[0] in vow: #Begins with vowel
            print(word + 'ay')
-        elif word[0] and word[1] in cons and word[2] in vow:
+        elif word[0] and word[1] in cons and word[2] in vow: #Word begins with consonant clasters (2 consonant)
             print(word[2:] + word[0:2] + 'ay')
-        elif word[0] in cons and word[1] in vow:
+        elif word[0] in cons and word[1] in vow: #Word begins with consonant 
             print(word[1:] + word[0] + 'ay')
-        elif word[0]and word[1] and word[2] in cons:
+        elif word[0]and word[1] and word[2] in cons: #Word begins with consonant clasters (3 consonant)
             print(word[3:] + word[0:3] + 'ay')
 else:
     print('слов нет!')
