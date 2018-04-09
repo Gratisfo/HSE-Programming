c33c21b340c2cae792e1ab44019765aa31e7b8a6
Gratisfo <31654704+Gratisfo@users.noreply.github.com>
2018-04-09 15:24:10 +0300

    Create task_1.py

---
+++ CONTROL_2/task_1.py
@@ -0,0 +1,23 @@
+import re
+def open_file(fname):
+  ''' Открывает и читает файл '''
+  with open (fname, encoding = 'utf-8') as f:
+    text = f.read()
+  return text
+
+def count_stripes(text):
+  '''Подсчитывает строки между <se> и <\se> и записывает их в файл'''
+  num_str = re.findall(r".*\n",text)
+  print(num_str)
+  num_str = len(re.findall(r".*\n",text))
+  
+  with open ('amount.txt', 'w', encoding = 'utf-8') as f:
+    f.write(str(num_str))
+    
+def main():
+  get_text = open_file('corpus.xml')
+  count = count_stripes(get_text)
+ 
+
+if __name__ == '__main__':
+    main()
