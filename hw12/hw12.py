eec0dbf7931d1a052aad4f39995cf56881a00e5b
Gratisfo <31654704+Gratisfo@users.noreply.github.com>
2018-04-22 23:38:39 +0300

    Update hw12.py

--- hw12/hw12.py
+++ hw12/hw12.py
@@ -10,6 +10,9 @@ def short_name(flist):
             dirs.append(file)
             if ' ' in file.lower(): #Есть пробелы в названии => больше одного слова
                 one_word.append(file)
+            if '_' in file.lower():
+                one_word.append(file)
+                
     if len(dirs) > 0:
         if len(one_word) > 0:
             print("Количество папок, в названии которых больше одного слова:", len(one_word), '\n', ', '.join(one_word))
